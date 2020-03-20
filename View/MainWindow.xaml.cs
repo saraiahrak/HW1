98cde54cbb822fba1cbc239c8ddb86d16bc4b72a
saraiahrak <saraiahrak99@gmail.com>
2020-03-20 16:46:47 +0200

    MainWindow

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HW1
+namespace HW1.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
